448540b4055539856f956386008e50206c44832d
oleh <olevdoshenko187@gmail.com>
2025-04-02 18:04:13 +0300

    oleh added print for test

--- bin/main.dart
+++ bin/main.dart
@@ -2,8 +2,6 @@ import 'package:payment_master/payment.dart';
 import 'package:payment_master/payment_method.dart';
 
 void main() {
-print('Hello, it is Tanya');
-print('Hello, it is Tanya');
   var cash = Payment.create(CashPaymentMethod(), 100.0);
   var card = Payment.create(CardPaymentMethod(), 250.50,
       cardNumber: '5363542105714969');
